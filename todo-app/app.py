128a5ee14870b0ca50a5964d6baf1eb30bb22ffc
jobatabs <2370315+jobatabs@users.noreply.github.com>
2025-06-25 14:19:26 +0300

    Ex 1.5.

--- todo-app/app.py
+++ todo-app/app.py
@@ -1,6 +1,6 @@
 import logging
 from os import getenv
-from flask import Flask
+from flask import Flask, render_template
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -15,5 +15,7 @@ app.config["PORT"] = getenv("PORT")
 test_env = getenv("TEST_ENV") == "true"
 app.logger.info(f"Test environment: {test_env}")
 app.logger.info(f"Server running on port {app.config['PORT']}")
-#if __name__ == "__main__":
-  #  app.run(port=app.config["PORT"], host="0.0.0.0", debug=test_env)
+
+@app.route("/")
+def index():
+    return render_template("index.html")
